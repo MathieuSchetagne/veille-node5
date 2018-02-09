fcdeab1d40adacc23c2acd903782071f8c4e7216
MathieuSchetagne <mathieu1166@me.com>
2018-02-09 13:09:05 -0500

    initialisation de lexercice

---
+++ 01_apps.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs'); 
+
+app.get('/',  (req, res) => {
+   res.send('Hello World');
+})
+
+const server = app.listen(8081, () => {
+   let host = server.address().address
+   let port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
