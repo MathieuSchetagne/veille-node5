5bb3d76028ae2662faa5cd859183bf71686ea1b1
MathieuSchetagne <mathieu1166@me.com>
2018-02-14 10:47:21 -0500

    ajout dans la base de données

--- 01_apps.js
+++ 01_apps.js
@@ -32,7 +32,6 @@ app.get('/formulaire',  (req, res) => {
   
         db.collection('adresse').save(req.body, (err, result) => {
         if (err) return console.log(err)
-        console.log('sauvegarder dans la BD')
         res.redirect('/')
         })
        
